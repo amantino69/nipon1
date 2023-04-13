076a839e74c0c6829aeae670b34a7f0aad482d62
amantino69 <amantino@yahoo.com>
2023-04-12 21:09:27 -0300

    Aprimorei o visual HTML e CSS das páginas do App

--- main.py
+++ main.py
@@ -1,51 +1,42 @@
-from __future__ import print_function
-from flask import Flask, render_template, request # Importa a biblioteca
-from itertools import count
+from flask import Flask, request
 from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
-import sys
-from pathlib import Path
 
-# Adicionar a pasta principal do projeto ao sys.path
-folder = Path(__file__).resolve().parent.parent
-sys.path.append(str(folder))
+app = Flask(__name__)
 
-
-
-app = Flask(__name__) # Inicializa a aplicação
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
+# Define as credenciais e autoriza a API do Google Calendar
 SCOPES = 'https://www.googleapis.com/auth/calendar'
 store = file.Storage('storage.json')
 creds = store.get()
 if not creds or creds.invalid:
     flow = client.flow_from_clientsecrets('C:/workspace/nipon1/app/dist/client_secret.json', SCOPES)
+    creds = tools.run_flow(flow, store)
 
-    creds = tools.run_flow(flow, store, flags) \
-        if flags else tools.run(flow, store)
 CAL = build('calendar', 'v3', http=creds.authorize(Http()))
 
-@app.route('/') # Nova rota
+@app.route('/')
 def main():
+    # Obter duas datas como parâmetros na URL
     primeira = request.args.get('primeira')
     segunda = request.args.get('segunda')
+
+    # Montar as strings de data/hora de início e fim do evento
     primeira = f"'{primeira}T13:00:00%s % GMT_OFF'"
     segunda = f"'{segunda}T13:00:00%s % GMT_OFF'"
 
-    GMT_OFF = '-04:00'          # ET/MST/GMT-4
+    GMT_OFF = '-04:00'
     EVENT1 = {
         'summary': 'Buy apples',
         'start': {'dateTime': primeira},
         'end': {'dateTime': segunda},
     }
 
+    # Criar um evento no Google Calendar e retorna "Hello, World" como resposta
     e = CAL.events().insert(calendarId='primary', sendNotifications=True, body=EVENT1).execute()
-     
+
     return "Hello, World"
 
 if __name__ == '__main__':
-  app.run(debug=True) # Executa a aplicação
+    app.run(debug=True)
+
